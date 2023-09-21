214ba639b6ee7b2c30064364133806102059c744
Ardelean Calin <9417983+Ardelean-Calin@users.noreply.github.com>
2023-09-21 14:45:37 +0300

    Added flake.

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    pkgs = import nixpkgs {system = "x86_64-linux";};
+  in {
+    devShells.x86_64-linux.default = pkgs.mkShell {
+      packages = [
+        # Made available on the CLI
+        pkgs.nodejs
+      ];
+    };
+
+    # You could also define extra shells or packages here
+  };
+}
